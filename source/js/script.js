2e00604a4905dda0a4822fab65c1ccb64765edf0
Алексей Иванов <Alessio18911@gmail.com>
2020-10-26 00:57:08 +0300

    Added swiper.js

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('Проверка корректности подключения файла');
+var mySwiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: false,
+
+  navigation: {
+    nextEl: '.form__controls-btn-next',
+    prevEl: '.form__controls-btn-prev',
+    disabledClass: 'form__controls-btn--disabled'
+  }
+});
+
+mySwiper.on('reachEnd', function() {
+  const sidebar = document.querySelector('.sidebar');
+  if (sidebar) sidebar.remove();
+
+});
